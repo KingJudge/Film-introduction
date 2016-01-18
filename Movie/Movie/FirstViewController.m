c3f6ae94f5c3aaac55dfec54fa277ce483cd1665
袁文轶 <ywy4869@live.cn>
2016-01-18 13:29:23 +0800

    no message

--- Movie/Movie/FirstViewController.m
+++ Movie/Movie/FirstViewController.m
@@ -8,7 +8,12 @@
 
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
+@interface FirstViewController ()<UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView *headerView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIPageControl *pageControl;
+@property (nonatomic, strong) NSTimer *timer;
 
 @end
 
@@ -16,14 +21,78 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    CGRect rect = _headerView.frame;
+    rect.size.height = self.view.frame.size.width / 2;
+    _headerView.frame = rect;
+    
+    _pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, rect.size.height - 40, rect.size.width, 40)];
+    _pageControl.pageIndicatorTintColor = [UIColor whiteColor];
+    _pageControl.currentPageIndicatorTintColor = [UIColor orangeColor];
+    _pageControl.numberOfPages = 6;
+    _pageControl.currentPage = 0;
+    [_headerView addSubview:_pageControl];
     if (self.navigationController.tabBarItem.tag == 0) {
         self.navigationItem.title = @"First";
     } else {
         self.navigationItem.title = @"Second";
     }
-
+    
+    CGFloat imageW = self.view.frame.size.width;
+    CGFloat imageH = rect.size.height;
+    CGFloat imageY = 0;
+    NSInteger totalCount = 6;
+    for (int i = 0; i < totalCount; i++) {
+        UIImageView *imageView = [[UIImageView alloc] init];
+        CGFloat imageX = i * imageW;
+        imageView.frame = CGRectMake(imageX, imageY, imageW, imageH);
+        imageView.clipsToBounds = YES;
+        //设置图片
+        NSString *name = [NSString stringWithFormat:@"shouye%d", i + 1];
+        imageView.image = [UIImage imageNamed:name];
+        self.scrollView.showsHorizontalScrollIndicator = NO;
+        [self.scrollView addSubview:imageView];
+    }
+    CGFloat contentW = totalCount *imageW;
+    self.scrollView.contentSize = CGSizeMake(contentW, rect.size.height);
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.delegate = self;
+    [self addTimer];
 }
+
+- (void)nextImage{
+    int page = (int)self.pageControl.currentPage;
+    if (page == 4) {
+        page = 0;
+    }else{
+        page++;
+    }
+    CGFloat x = page * _headerView.frame.size.width;
+    self.scrollView.contentOffset = CGPointMake(x, 0);
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGFloat scrollviewW = _headerView.frame.size.width;
+    CGFloat x = scrollView.contentOffset.x;
+    int page = (x + scrollviewW / 2) /  scrollviewW;
+    self.pageControl.currentPage = page;
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+    [self removeTimer];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+    [self addTimer];
+}
+
+- (void)addTimer{
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(nextImage) userInfo:nil repeats:YES];
+}
+
+- (void)removeTimer{
+    [self.timer invalidate];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- Movie/Movie/TabViewController.m
+++ Movie/Movie/TabViewController.m
@@ -24,12 +24,12 @@
     //将_firstVC设置为_firstNC导航控制器的跟视图
     _firstNC = [[UINavigationController alloc]initWithRootViewController:_firstVC];
     //为_firstNC导航控制器设置tab bar item，将该选项卡栏项目的标题设置为first,图片设置为名为first的图片，下表设为0
-    _firstNC.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"First" image:[UIImage imageNamed:@"First"] tag:0];
+    _firstNC.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"首页" image:[UIImage imageNamed:@"First"] tag:0];
     
     UIStoryboard *storyboard2 = [UIStoryboard storyboardWithName:@"Second" bundle:[NSBundle mainBundle]];
     _SecondVC = [storyboard2 instantiateViewControllerWithIdentifier:@"Second"];
     _SecondNC = [[UINavigationController alloc]initWithRootViewController:_SecondVC];
-    _SecondNC.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"Second" image:[UIImage imageNamed:@"Second"] tag:1];
+    _SecondNC.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"分类" image:[UIImage imageNamed:@"Second"] tag:1];
     //设置选项卡栏控制器的选项卡栏项目（@［］数组中的每个item都会对应一个选项卡栏项目）
     self.viewControllers = @[_firstNC,_SecondNC];
 }
